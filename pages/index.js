e6f60b6e3d105efb1cad9e383a81b45215ca9ddb
Jimmy200 <bruno_pier@hotmail.com>
2021-05-28 13:04:52 -0300

    Adds Head title in index

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
+import Head from 'next/head';
+
 import Container from '../components/Container';
 
 const Index = () => {
   return (
     <Container>
-      <h1>Index</h1>
+      <Head>
+        <title>Next.js - Home</title>
+      </Head>
+        <h1>Index</h1>
     </Container>
   );
 };
