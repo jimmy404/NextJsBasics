a6ff07f003978ded8bb2c27d01cf5e34c84f2c11
Jimmy200 <bruno_pier@hotmail.com>
2021-05-28 12:53:58 -0300

    Adds container to render navigation - page head - styles - in services

--- pages/services.js
+++ pages/services.js
@@ -1,5 +1,10 @@
-const Services = () => {
-  return <h1>Hello World - Services</h1>
-};
+import Container from '../components/Container';
+
+const Services = () => (
+  <Container>
+    <h1>Services</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit cupiditate iusto beatae labore dicta rerum voluptates adipisci at aperiam iure, in voluptatum sint officiis hic illum architecto dolorum, officia consectetur?</p>
+  </Container>
+);
 
 export default Services;
