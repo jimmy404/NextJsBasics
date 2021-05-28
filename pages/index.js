8f3b2e7d98ecf34e116fdf93dd45c7abe3203703
Jimmy200 <bruno_pier@hotmail.com>
2021-05-28 15:54:25 -0300

    Adds getInitialProps to fetch data - index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,25 @@
 import Head from 'next/head';
+import fetch from 'isomorphic-fetch';
 
 import Container from '../components/Container';
+import Users from '../components/Users';
 
-const Index = () => {
+const Index = (props) => {
   return (
     <Container>
       <Head>
         <title>Next.js - Home</title>
       </Head>
-        <h1>Index</h1>
+        <h1>Next Js</h1>
+        <Users users={props.users}/>
     </Container>
   );
+};
+
+Index.getInitialProps = async (ctx) => {
+  const res = await fetch('https://reqres.in/api/users');
+  const resJSON = await res.json();
+  return {users: resJSON.data};
 };
 
 export default Index;
