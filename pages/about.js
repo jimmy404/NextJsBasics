6db99add442dba7f05e2da47709b71c080874609
Jimmy200 <bruno_pier@hotmail.com>
2021-05-28 13:05:06 -0300

    Adds Head title in about

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,13 @@
+import Head from 'next/head';
+
 import Container from '../components/Container';
 
 const About = () => {
   return (
     <Container>
+    <Head>
+      <title>Next.js - About</title>
+    </Head>
       <h1>About</h1>
     </Container>
   );
