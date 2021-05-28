405c86a33461385553223803d970bfa8cd776758
Jimmy200 <bruno_pier@hotmail.com>
2021-05-28 16:10:25 -0300

    Adds user image & styles

--- components/Users.js
+++ components/Users.js
@@ -5,9 +5,16 @@ const Users = (props) => {
     <ul className="list-group">
       {
         props.users.map(user => (
-          <li className="list-group-item list-group-item-action" key={user.id}>
-            <h5>{user.first_name} {user.last_name}</h5>
-            <p>Email: {user.email}</p>
+          <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-center" key={user.id}>
+            <div>
+              <h5>{user.first_name} {user.last_name}</h5>
+              <p>Email: {user.email}</p>
+            </div>
+            <img
+              src={user.avatar}
+              alt={user.first_name + user.last_name}
+              style={{borderRadius: '50%'}}
+            />
           </li>
         ))
       }
