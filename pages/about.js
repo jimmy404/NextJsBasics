268f96de2d664a80f4396df4dd21d674a09f2b98
Jimmy200 <bruno_pier@hotmail.com>
2021-05-28 12:28:56 -0300

    Adds Navigation component in about-index

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Navigation from '../components/Navigation';
+
+const About = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>About</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Navigation from '../components/Navigation';
+
+const Index = () => {
+  return (
+    <div>
+      <Navigation />
+      <h1>Index</h1>
+  </div>
+  );
+};
+
+export default Index;
