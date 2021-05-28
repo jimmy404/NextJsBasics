b73ad991cf776dd960e42fb1a7243d12f97d12e3
Jimmy200 <bruno_pier@hotmail.com>
2021-05-28 12:52:24 -0300

    Adds Head for pages & Styles link - in Container file

--- components/Container.js
+++ components/Container.js
@@ -1,7 +1,12 @@
+import Head from 'next/head';
 import Navigation from './Navigation';
 
 const Container = (props) => (
   <div>
+    <Head>
+      <title>Next Js Basics</title>
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@4.5.2/dist/superhero/bootstrap.min.css" />
+    </Head>
     <Navigation />
     <div>
       {props.children}
