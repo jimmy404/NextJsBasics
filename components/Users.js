3fc33c8ee9a2c436dd2e3b8864d6361a252ceb32
Jimmy200 <bruno_pier@hotmail.com>
2021-05-28 16:32:37 -0300

    Adds the router for individual user selection

--- components/Users.js
+++ components/Users.js
@@ -1,11 +1,11 @@
+import Router from 'next/router';
 
 const Users = (props) => {
-  console.log(props)
   return (
     <ul className="list-group">
       {
         props.users.map(user => (
-          <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-center" key={user.id}>
+          <li className="list-group-item list-group-item-action d-flex justify-content-between align-items-center" key={user.id} onClick={e => Router.push('/users/[id]', `/users/${user.id}`)}>
             <div>
               <h5>{user.first_name} {user.last_name}</h5>
               <p>Email: {user.email}</p>
