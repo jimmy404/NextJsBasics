57f0cf34cecbce23b4fb4b06557b369e41eca397
Jimmy200 <bruno_pier@hotmail.com>
2021-05-28 13:05:15 -0300

    Adds Head title in services

--- pages/services.js
+++ pages/services.js
@@ -1,7 +1,12 @@
+import Head from 'next/head';
+
 import Container from '../components/Container';
 
 const Services = () => (
   <Container>
+    <Head>
+      <title>Next.js - Services</title>
+    </Head>
     <h1>Services</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit cupiditate iusto beatae labore dicta rerum voluptates adipisci at aperiam iure, in voluptatum sint officiis hic illum architecto dolorum, officia consectetur?</p>
   </Container>
